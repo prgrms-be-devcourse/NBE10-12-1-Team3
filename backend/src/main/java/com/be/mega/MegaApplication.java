ac78ba9b11f1e1dd6c0b18b107ae132aab6082ec
bangyewon <byewon1111@gmail.com>
2026-06-05 12:30:24 +0900

    chore:프로젝트 초기 셋팅

---
+++ backend/src/main/java/com/be/mega/MegaApplication.java
@@ -0,0 +1,13 @@
+package com.be.mega;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MegaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MegaApplication.class, args);
+	}
+
+}
