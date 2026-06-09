91c869745542e5fabc07585137bd8eeb5888197e
crispy <brian3092@naver.com>
2026-06-09 16:45:54 +0900

    refactor: 코드리뷰 반영- 중첩 record 수정, Annotation 및 Validation 추가, N+1 문제 해결#8

--- backend/src/main/java/com/be/mega/controller/OrderController.java
+++ backend/src/main/java/com/be/mega/controller/OrderController.java
@@ -4,8 +4,8 @@ import com.be.mega.common.apiResponse.CustomApiResponse;
 import com.be.mega.dto.request.OrderSearchRequest;
 import com.be.mega.dto.response.OrderSearchResponse;
 import com.be.mega.service.OrderService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,13 +16,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class OrderController {
     private final OrderService orderService;
-    @PostMapping("/search")
-    public ResponseEntity<CustomApiResponse<OrderSearchResponse.OrderCoverResponse>> getOrders(
-            @RequestBody OrderSearchRequest request) {
-        OrderSearchResponse.OrderCoverResponse response = orderService.getOrdersByEmail(request.email());
 
-        return ResponseEntity.ok(
-                CustomApiResponse.success(response, 200 , "사용자 주문 전체 조회 성공")
-        );
+    @PostMapping("/search")
+    public CustomApiResponse<OrderSearchResponse> getOrders(
+            @Valid @RequestBody OrderSearchRequest request) {
+        OrderSearchResponse response = orderService.getOrdersByEmail(request.email());
+
+        return CustomApiResponse.success(response, 200 , "사용자 주문 전체 조회 성공");
+
     }
 }
