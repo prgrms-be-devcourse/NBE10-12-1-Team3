64abed7ff3f96a98f5c6812edef4bd90cb718f99
crispy <brian3092@naver.com>
2026-06-09 16:45:54 +0900

    refactor: 코드리뷰 반영- 중첩 record 수정, Annotation 및 Validation 추가, N+1 문제 해결#8

--- backend/src/main/java/com/be/mega/repository/OrderRepository.java
+++ backend/src/main/java/com/be/mega/repository/OrderRepository.java
@@ -2,9 +2,11 @@ package com.be.mega.repository;
 
 import com.be.mega.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
     List<Order> findByEmail(String email);
 }
