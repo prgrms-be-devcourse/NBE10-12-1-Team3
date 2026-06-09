e46af18ba3fd00eba23e9204146c82728277dc9d
SOL4R1S <mingh662@gmail.com>
2026-06-09 16:37:59 +0900

    refactor: 상품 조회 계층 책임 정리 #4

--- backend/src/main/java/com/be/mega/repository/ProductRepository.java
+++ backend/src/main/java/com/be/mega/repository/ProductRepository.java
@@ -3,7 +3,9 @@ package com.be.mega.repository;
 
 import com.be.mega.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
 }
