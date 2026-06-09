be73455da19a7fc4eed1966e480008c302bd7d58
crispy <brian3092@naver.com>
2026-06-09 10:20:28 +0900

    refactor: 주석 제거 및 단방향 관계 재설정#1

--- backend/src/main/java/com/be/mega/entity/Order.java
+++ backend/src/main/java/com/be/mega/entity/Order.java
@@ -2,17 +2,15 @@ package com.be.mega.entity;
 
 import com.be.mega.enums.PostStatus;
 import com.be.mega.common.domain.BaseEntity;
-import com.be.mega.entity.OrderItem;
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.ColumnDefault;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 
 
 @Entity
-@Table(name = "orders") // order는 SQL 예약어라 orders로
+@Table(name = "orders")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
@@ -41,27 +39,16 @@ public class Order extends BaseEntity {
     @Column(name = "order_number", unique = true, nullable = false)
     private String orderNumber;
 
-    @Column(name = "order_deletedAt")
-    private LocalDateTime orderDeletedAt;
+    @Column(name = "deleted_at")
+    @ColumnDefault("null")
+    private LocalDateTime deletedAt;
 
-    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL , orphanRemoval = true)
-    private List<OrderItem> orderItems = new ArrayList<>();
-
-    // === 연관관계 편의 메서드 ===
-    public void addOrderItem(OrderItem orderItem) {
-        this.orderItems.add(orderItem); // Order -> OrderItem 등록 (상품목록)
-        orderItem.assignOrder(this); // OrderItem -> Order 등록 (상품목록의 주문번호)
-    }
-
-    // === Soft Delete ===
-    public void delete() {
-        this.orderDeletedAt = LocalDateTime.now(); // deletedAt 업데이트
+    public void orderDelete() {
+        this.deletedAt = LocalDateTime.now();
     }
 
     public boolean isDeleted() {
-        return this.orderDeletedAt != null; // 삭제된 레코드인지
+        return this.deletedAt != null;
     }
-
-
 
 }
