45d1d7243a8a9931b9a3300597b3b48c1fcbca84
bangyewon <byewon1111@gmail.com>
2026-06-10 10:01:43 +0900

    refactor: 스웨거 태그 추가 #2

--- backend/src/main/java/com/be/mega/controller/AdminController.java
+++ backend/src/main/java/com/be/mega/controller/AdminController.java
@@ -5,6 +5,8 @@ import com.be.mega.dto.request.EmailVerifyRequest;
 import com.be.mega.dto.response.OrderListResponse;
 import com.be.mega.entity.enums.PostStatus;
 import com.be.mega.service.AdminService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
@@ -17,16 +19,19 @@ import org.springframework.web.bind.annotation.*;
 @Validated
 @RequiredArgsConstructor
 @RequestMapping("/v1/admin")
+@Tag(name = "관리자 페이지 컨트롤러",description = "관리자 페이지 관련 기능 API 입니다.")
 public class AdminController {
     private final AdminService adminService;
 
     @PostMapping("/verify-email")
+    @Operation(summary = "관리자 이메일 검증", description = "관리자 이메일 검증 API")
     public CustomApiResponse<Void> verifyAdmin(@Valid @RequestBody EmailVerifyRequest request) {
         adminService.verifyAdmin(request);
         return CustomApiResponse.success(null, 200, "인증 및 조회 성공");
     }
 
     @GetMapping("/orders")
+    @Operation(summary = "관리자 주문 조회", description = "관리자가 주문 조회 및 검색 API")
     public CustomApiResponse<OrderListResponse> showOrders(
             @RequestParam(defaultValue = "0")
             @Min(value = 0, message = "page는 0 이상이어야 합니다.")
